a06e31c910c8ad4e728deaa945f013a243e70dcc
Yale Huang <calvino.huang@gmail.com>
2023-03-26 22:05:34 +0800

    Added tests.

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -4,12 +4,19 @@ import * as assert from 'assert';
 // as well as import your extension to test it
 import * as vscode from 'vscode';
 // import * as myExtension from '../../extension';
+import * as chatgptai from '../../extension';
 
 suite('Extension Test Suite', () => {
 	vscode.window.showInformationMessage('Start all tests.');
 
-	test('Sample test', () => {
-		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
-	});
+	// test('Sample test', () => {
+	// 	assert.strictEqual(-1, [1, 2, 3].indexOf(5));
+	// 	assert.strictEqual(-1, [1, 2, 3].indexOf(0));
+	// });
+
+    test('Extension was loaded', async () => {
+        await vscode.extensions.getExtension('YaleHuang.chatgpt-ai')?.activate();
+		const extension = vscode.extensions.getExtension('YaleHuang.chatgpt-ai');
+		assert.ok(extension, 'Extension is not installed.');
+    });
 });
